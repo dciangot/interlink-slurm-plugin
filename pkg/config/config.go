822ce86ef2686920ff0ff75e3eff1937be4392c6
dciangot <dciangot@cern.ch>
2025-10-16 15:12:13 +0200

    Remove the excluded backend imports from config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,9 +6,6 @@ import (
 
 	"gopkg.in/yaml.v3"
 
-	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/containerd"
-	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/docker"
-	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/htcondor"
 	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/podman"
 	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/slurm"
 )
