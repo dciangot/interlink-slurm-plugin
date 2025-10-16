87a0e8d1ddde1e40a627126047dfc371d62f6581
dciangot <dciangot@cern.ch>
2025-10-16 21:23:57 +0200

    Removing conditional backend imports

--- cmd/main.go
+++ cmd/main.go
@@ -25,9 +25,6 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend"
-	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/containerd"
-	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/docker"
-	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/htcondor"
 	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/podman"
 	slurmbackend "github.com/intertwin-eu/interlink-slurm-plugin/pkg/backend/slurm"
 	"github.com/intertwin-eu/interlink-slurm-plugin/pkg/config"
