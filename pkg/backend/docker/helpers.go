82955c6e08b355060bf1faad9f1640940d599538
dciangot <dciangot@cern.ch>
2025-10-16 14:40:18 +0200

    Update docker imports to use moby/moby in helpers.go

--- pkg/backend/docker/helpers.go
+++ pkg/backend/docker/helpers.go
@@ -11,9 +11,9 @@ import (
 	"time"
 
 	"github.com/containerd/containerd/log"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/mount"
-	dockerclient "github.com/docker/docker/client"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/api/types/mount"
+	dockerclient "github.com/moby/moby/client"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
