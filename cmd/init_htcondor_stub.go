b88c993580371ac50f4c9cfc7597f185e3fcc3de
dciangot <dciangot@cern.ch>
2025-10-16 21:25:59 +0200

    Fixing unused parameter in stub function

--- cmd/init_htcondor_stub.go
+++ cmd/init_htcondor_stub.go
@@ -15,6 +15,6 @@ func initHTCondorBackend(ctx context.Context, cfg *config.UnifiedConfig) (backen
 	return nil, fmt.Errorf("HTCondor backend not compiled in this build")
 }
 
-func getHTCondorLogging(cfg *config.UnifiedConfig) (verboseLogging, errorsOnlyLogging bool) {
+func getHTCondorLogging(_ *config.UnifiedConfig) (verboseLogging, errorsOnlyLogging bool) {
 	return false, false
 }
